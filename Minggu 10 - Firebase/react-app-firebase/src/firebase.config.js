167bfb80c8c3d47c5dc1778284eed006c9463084
Erninda <44139473+Erninda@users.noreply.github.com>
2021-04-20 19:22:10 +0700

    update

---
+++ Minggu 10 - Firebase/react-app-firebase/src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+        apiKey: "AIzaSyDZzWilHo9b5DwE5QLIdWbiFMJ7Aih7KvY",
+        authDomain: "ornate-keel-304301.firebaseapp.com",
+        projectId: "ornate-keel-304301",
+        storageBucket: "ornate-keel-304301.appspot.com",
+        messagingSenderId: "616646524188",
+        appId: "1:616646524188:web:eff2af14dcef2e66fc0ed9",
+        measurementId: "G-TT9Q1Y9KY1"
+};
---
+++ Minggu 10 - Firebase/react-app-firebase/src/index.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
+// import './index.css';
+import './style.css';
+import Header from "./Header"
+import routes from "./routes"
+import firebase from "firebase";
+import firebaseConfig from "./firebase.config";
+
+firebase.initializeApp(firebaseConfig);
+
+export const AuthContext = React.createContext(null);
+
+function App() {
+  const [isLoggedIn, setLoggedIn] = useState(false);
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, setLoggedIn }}>
+
+      <div className="App">
+        <Router>
+          <Header />
+
+          <p id="notif">
+            Is logged in? {JSON.stringify(isLoggedIn)}
+          </p>
+
+          <Switch>
+            {routes.map(route => (
+              <Route key={route.path} path={route.path} exact={route.exact} component={route.main} />
+            ))}
+          </Switch>
+        </Router>
+      </div>
+    </AuthContext.Provider>
+  )
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
